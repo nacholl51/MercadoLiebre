2466cd32c2337f0c9b2906a5ed21ef5950f335c5
nacholl51 <ignaciollamas51@hotmail.com>
2022-01-19 19:49:11 -0300

    Arregle algunas cosas por que no estaba bien hecho el deply de heroku

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+const path = require('path')
 
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server listening on port 3000")
+})
 
-app.listen(preccess.env.PORT || 3000, ()=>{
-    console.log('Servidor funcionando');
-});
+app.use(express.static(path.join(__dirname,"/public")))
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get('/contacto', (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/contacto.html"))
+})
